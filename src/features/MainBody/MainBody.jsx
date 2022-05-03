ebef41ee816cb85b1daca15d76dcd4800b757d24
Rob Pelance <rpelance@gmail.com>
2022-05-03 00:21:53 -0400

    Implement posts fetching

--- src/features/MainBody/MainBody.jsx
+++ src/features/MainBody/MainBody.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { 
+    selectFilteredPosts, 
+    fetchPosts, 
+    fetchComments,
+    setSearchTerm,
+ } from '../../store/redditSlice';
 
 export function MainBody() {
+    const reddit = useSelector((state) => state.reddit);
+    const { isLoading, error, searchTerm, selectedSubreddit } = reddit;
+    const dispatch = useDispatch();
+    const posts = useSelector(selectFilteredPosts);
+
+    useEffect(() => {
+        dispatch(fetchPosts(selectedSubreddit));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedSubreddit]);
+
+    const onToggleComments = (index) => {
+        const getComments = (permalink) => {
+            dispatch(fetchComments(index, permalink));
+        };
+
+        return getComments;
+    }
     
+    if (error) {
+        return (
+            <div className='error'>
+                <h2>Failed to load posts.</h2>
+                <button
+                    type='button'
+                    onClick={() => dispatch(fetchPosts(selectedSubreddit))}
+                >
+                    Try Again
+                </button>
+            </div>
+        );
+    }
+
+    if (posts.length === 0) {
+        return (
+            <div className='error'>
+                <h2>No posts matching "{searchTerm}"</h2>
+                <button type='button' onClick={() => dispatch(setSearchTerm(''))}>
+                    Go Home
+                </button>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {posts.map((post, index) => (
+                <div>
+                    <p>Test</p>
+                </div>
+            ))}
+        </>
     );
 };
--- src/store/redditSlice.js
+++ src/store/redditSlice.js
@@ -1,17 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSelector, createSlice } from '@reduxjs/toolkit';
+import { getSubredditPosts, getPostComments } from '../api/reddit';
 
 const initialState = {
     posts: [],
     error: false,
     isLoading: false,
     searchTerm: '',
-    selectedSubreddit: '',
+    selectedSubreddit: '/r/mildlyinteresting/',
 };
 
 const redditSlice = createSlice({
     name: 'redditPosts',
     initialState,
     reducers: {
+        startGetPosts(state) {
+            state.isLoading = true;
+            state.error = false;
+        },
+        getPostsSuccess(state, action) {
+            state.isLoading = false;
+            state.posts = action.payload;
+        },
+        getPostsFailed(state) {
+            state.isLoading = false;
+            state.error = true;
+        },
         setSearchTerm(state, action) {
             state.searchTerm = action.payload;
         },
@@ -19,14 +32,80 @@ const redditSlice = createSlice({
             state.selectedSubreddit = action.payload;
             state.searchTerm = '';
         },
+        startGetComments(state, action) {
+            state.posts[action.payload].showingComments = !state.posts[action.payload].showingComments;
+            if (!state.posts[action.payload].showingComments) {
+                return;
+            }
+            state.posts[action.payload].loadingComments = true;
+            state.posts[action.payload].error = false;
+        },
+        getCommentsSuccess(state, action) {
+            state.posts[action.payload.index].loadingComments = false;
+            state.posts[action.payload.index].comments = action.payload.comments;
+        },
+        getCommentsFailed(state, action) {
+            state.posts[action.payload].loadingComments = false;
+            state.posts[action.payload].error = true;
+        },
     },
 });
 
 export const {
+    startGetPosts,
+    getPostsSuccess,
+    getPostsFailed,
     setSearchTerm,
     setSelectedSubreddit,
+    startGetComments,
+    getCommentsSuccess,
+    getCommentsFailed,
 } = redditSlice.actions;
 
 export default redditSlice.reducer;
 
+export const fetchPosts = (subreddit) => async (dispatch) => {
+    try {
+        dispatch(startGetPosts());
+        const posts = await getSubredditPosts(subreddit);
+
+        const postsWithMetaData = posts.map((post) => ({
+            ...post,
+            showingComments: false,
+            comments: [],
+            loadingComments: false,
+            errorComments: false,
+        }));
+        dispatch(getPostsSuccess(postsWithMetaData));
+    } catch (error) {
+        dispatch(getPostsFailed());
+    };
+};
+
+export const fetchComments = (index, permalink) => async (dispatch) => {
+    try {
+        dispatch(startGetComments(index));
+        const comments = await getPostComments(permalink);
+        dispatch(getCommentsSuccess({ index, comments }));
+    } catch (error) {
+        dispatch(getCommentsFailed(index));
+    };
+};
+
+const selectPosts = (state) => state.reddit.posts;
+const selectSearchTerm = (state) => state.reddit.searchTerm;
+
 export const selectSelectedSubreddit = (state) => state.reddit.selectedSubreddit;
+
+export const selectFilteredPosts = createSelector(
+    [selectPosts, selectSearchTerm],
+    (posts, searchTerm) => {
+        if (searchTerm !== '') {
+            return posts.filter((post) =>
+                post.title.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+        }
+
+        return posts;
+    }
+)
